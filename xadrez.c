188fca262b3c4f12266534bbedd2f03b65e4f61c
matheusmb1999 <matheusbueno9996@hotmail.com>
2025-05-27 21:28:34 +0000

    Nivel 3 - Rev

--- xadrez.c
+++ xadrez.c
@@ -3,6 +3,7 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// Revisado 1
 
 // Função recursiva #1: Bispo
 void moverBispo(int casas) {
